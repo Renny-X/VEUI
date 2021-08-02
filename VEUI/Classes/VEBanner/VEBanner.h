229aa61183ad21af76308c0c34c0369c12c64e13
Coder <tmxuhan5@qq.com>
2021-08-02 15:25:25 +0800

    【VEBanner】属性类型修复

--- VEUI/Classes/VEBanner/VEBanner.h
+++ VEUI/Classes/VEBanner/VEBanner.h
@@ -44,7 +44,7 @@ NS_ASSUME_NONNULL_BEGIN
 
 @property(nonatomic, assign)BOOL showPageControl;
 
-@property(nonatomic, assign)BOOL pageControlBottomOffset;
+@property(nonatomic, assign)CGFloat pageControlBottomOffset;
 
 @property(nonatomic, assign)UIColor *tintColor;
 /**
